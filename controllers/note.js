8e452dad1e451c104ac440fb1d46a470125394aa
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-01-04 12:32:10 +0630

    authorization

--- controllers/note.js
+++ controllers/note.js
@@ -17,7 +17,7 @@ export const index = async (req, res) => {
 export const create = async (req, res) => {
   try {
     const note = await Note.create(req.body);
-    res
+    return res
       .status(StatusCodes.CREATED)
       .json(successResponse(StatusCodes.CREATED, "Create Note Success", note));
   } catch (error) {
@@ -39,9 +39,36 @@ export const show = async (req, res) => {
       .status(StatusCodes.NOT_FOUND)
       .json(errorResponse(StatusCodes.NOT_FOUND, "Note not found"));
   }
-  res
+  return res
     .status(StatusCodes.ACCEPTED)
     .json(successResponse(StatusCodes.ACCEPTED, "Fetch Message Success", note));
+};
+
+export const update = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updateNote = await Note.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updateNote) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json(errorResponse(StatusCodes.NOT_FOUND, "Note not found"));
+    }
+    return res
+      .status(StatusCodes.ACCEPTED)
+      .json(successResponse(StatusCodes.ACCEPTED, "Note Updated", updateNote));
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(
+        errorResponse(
+          StatusCodes.INTERNAL_SERVER_ERROR,
+          "Internal Server Error"
+        )
+      );
+  }
 };
 
 export const deleteNote = async (req, res) => {
@@ -49,12 +76,12 @@ export const deleteNote = async (req, res) => {
   try {
     await Note.findByIdAndDelete(id);
 
-    res
+    return res
       .status(StatusCodes.NO_CONTENT)
       .json(StatusCodes.NO_CONTENT, "Note Deleted", []);
   } catch (error) {
     console.log(error);
-    res
+    return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json(
         successResponse(
